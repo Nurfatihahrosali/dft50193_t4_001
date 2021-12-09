4c8051a349278e8b75a7a5165ed4ae6f2852c173
Nurfatihahrosali <fatihaheren12@gmail.com>
2021-12-09 08:33:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,13 @@
 <html>
 <body>
 
+
+
 <?php
 $myfile = fopen("courses.txt", "r") or die("Unable to open file!");
-echo fgets($myfile);
+echo nl2br(fread($myfile,filesize("courses.txt")));
 fclose($myfile);
 ?>
-
-<?php/*
-$myfile = fopen("courses.txt", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("courses.txt"));
-fclose($myfile);
-*/?>
 
 
 
